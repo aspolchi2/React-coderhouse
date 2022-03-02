95f2a1629551f6de6dc339169aaa2994c18aa038
Agustin Somoza Polchi <aspolchi@gmail.com>
2022-03-02 20:37:50 -0300

    empieza

---
+++ clase 1/index.js
@@ -0,0 +1,11 @@
+class auto{
+    constructor(marca, modelo, puerta){
+        this.marca = marca;
+        this.modelo = modelo;
+        this.puerta = puerta;
+    }
+    mostrarAuto (){
+        console.log(`${this.marca} ${this.modelo}`);
+    }
+    
+}
